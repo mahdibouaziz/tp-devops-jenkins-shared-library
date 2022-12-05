20a94484670fee89c91ab4a02ba4329214a8c879
mahdibouaziz <mahdibouaziz@hotmail.fr>
2022-12-05 11:05:50 +0100

    separated the build and the push

--- vars/buildDockerImageAndPushDockerHub.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-
-import com.sharedfunctions.Docker
-def call(String imageName){
-    return new Docker(this).buildDockerImage(imageName)
-}
---
+++ vars/dockerHubPush.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+def call(String imageName){
+    echo 'building the Docker image ...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
+        def user = env.DOCKER_USER
+        def password = env.DOCKER_PASS
+        //Initializing a global variable.
+        sh "docker build -t $imageName ."
+        echo "$password | docker login -u $user --password-stdin"
+        sh "docker push $imageName"
+    }
+}
