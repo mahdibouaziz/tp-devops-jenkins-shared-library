d2815812635014de5610aae07b177364cbd7313b
mahdibouaziz <mahdibouaziz@hotmail.fr>
2022-12-05 10:27:03 +0100

    added shared library

--- vars/buildDockerImageAndPushDockerHub.groovy
+++ vars/buildDockerImageAndPushDockerHub.groovy
@@ -1,14 +1,14 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo 'building the Docker image ...'
 
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
         def user = env.DOCKER_USER
         def password = env.DOCKER_PASS
-        //Initializing a global variable. Notice there is no def here
-        sh "docker build -t mahdibouaziz/private:DevopsDS-2.0 ."
+        //Initializing a global variable.
+        sh "docker build -t $imageName ."
         echo "$password | docker login -u $user --password-stdin"
-        sh "docker push mahdibouaziz/private:DevopsDS-2.0"
+        sh "docker push $imageName"
     }
 }
--- vars/buildDockerImageAndPushNexus.groovy
+++ vars/buildDockerImageAndPushNexus.groovy
@@ -1,15 +1,15 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo 'building the Docker image To Nexus ...'
 
     withCredentials([usernamePassword(credentialsId: 'nexus-repo', passwordVariable: 'NEXUS_PASS', usernameVariable: 'NEXUS_USER')]) {
         def user = env.NEXUS_USER
         def password = env.NEXUS_PASS
         //Initializing a global variable. Notice there is no def here
-        sh "docker build -t 192.168.100.162:8083/project:DevopsDS-1.1 ."
+        sh "docker build -t $imageName ."
         echo "$password | docker login -u $user --password-stdin 192.168.100.162:8083"
-        sh "docker push 192.168.100.162:8083/project:DevopsDS-1.1"
+        sh "docker push $imageName"
     }
 }
 
