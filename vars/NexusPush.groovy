402a56321ce4fa512c6032984a1cb83377f895d4
mahdibouaziz <mahdibouaziz@hotmail.fr>
2022-12-05 11:13:02 +0100

    added the nexus push

--- vars/NexusPush.groovy
+++ vars/NexusPush.groovy
@@ -1,14 +1,14 @@
 #!/usr/bin/env groovy
 
-def call(String imageName){
-    echo 'building the Docker image To Nexus ...'
+def call(String imageName, String nexusIP){
+    echo 'Push image To Nexus ...'
 
     withCredentials([usernamePassword(credentialsId: 'nexus-repo', passwordVariable: 'NEXUS_PASS', usernameVariable: 'NEXUS_USER')]) {
         def user = env.NEXUS_USER
         def password = env.NEXUS_PASS
         //Initializing a global variable. Notice there is no def here
         sh "docker build -t $imageName ."
-        echo "$password | docker login -u $user --password-stdin 192.168.100.162:8083"
+        echo "$password | docker login -u $user --password-stdin $nexusIP"
         sh "docker push $imageName"
     }
 }
